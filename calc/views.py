b429463974f5d91f3f62e6ebee9724e2006a7244
piyush55 <piyushshri55@gmail.com>
2020-04-28 23:37:06 +0530

    initial commit

---
+++ calc/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+# from django.http import HttpResponse 
+from .models import Result
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html', {'name': 'shubham'})
+
+def add(request):
+    result = "weather is good"
+    return render (request, 'result.html', {'result': result})
